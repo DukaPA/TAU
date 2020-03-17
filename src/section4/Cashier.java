ae90bf9e7012a54b9cc2fb355811d2994f40094a
Dusan Veljkovic <dusan.veljkovic.81@gmail.com>
2020-03-17 15:15:10 +0100

    loops

---
+++ src/section4/Cashier.java
@@ -0,0 +1,22 @@
+package section4;
+
+import java.util.Scanner;
+
+public class Cashier {
+    public static void main(String[] args) {
+        System.out.println("Enter number of items you would like to scan: ");
+        Scanner scanner = new Scanner(System.in);
+        int quantity = scanner.nextInt();
+
+        double total = 0;
+
+        for(int i = 0; i < quantity; i++){
+            System.out.println("Enter the cost of item: ");
+            double price = scanner.nextDouble();
+
+            total = total + price;
+        }
+        scanner.close();
+        System.out.println("Yout toal is $" + total);
+    }
+}
