688b66050eb55754f9f2d6f0c65b2048804b184d
Dusan Veljkovic <dusan.veljkovic.81@gmail.com>
2020-04-05 03:47:11 +0200

    data types and strings

---
+++ src/section8/PasswordValidator.java
@@ -0,0 +1,11 @@
+package section8;
+
+public class PasswordValidator {
+    /**
+     * Validate password, at least 8 characters long,
+     * contain uppercase letter,
+     * contain special character,
+     * not contain the username,
+     * not the same as the old password
+     */
+}
