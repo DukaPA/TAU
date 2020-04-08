8cb001c6b3c0528fb4628a3a4e46409a8c592dd8
Dusan Veljkovic <dusan.veljkovic.81@gmail.com>
2020-04-09 01:31:38 +0200

    Inheritance

---
+++ src/section9/InheritanceTester.java
@@ -0,0 +1,11 @@
+package section9;
+
+public class InheritanceTester {
+
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        Employee employee = new Employee();
+
+    }
+}
